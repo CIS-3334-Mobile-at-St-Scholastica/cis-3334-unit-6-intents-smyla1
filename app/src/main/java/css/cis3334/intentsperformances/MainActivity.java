f59e79073d44dab74b7b497a26d772f3635b2573
smyla1 <smyla@css.edu>
2020-02-22 04:44:28 -0600

    Unit6 Practice code developed by Sumathilatha

--- app/src/main/java/css/cis3334/intentsperformances/MainActivity.java
+++ app/src/main/java/css/cis3334/intentsperformances/MainActivity.java
@@ -1,6 +1,8 @@
 package css.cis3334.intentsperformances;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +11,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.GregorianCalendar;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,8 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
         textViewStatus = (TextView) findViewById(R.id.textViewStatus);
         setupButtonClickEvents();
-
-
+        setupButtonClickEvents2();
+        setupButtonClickEvents3();
 
     }
 
@@ -37,6 +40,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Do something in response to button click
                 textViewStatus.setText("Code should display website for performance 1");
+                Uri webpage = Uri.parse("http://www.css.edu/about/spotlight-arts-and-lectures.html");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+
+                PackageManager packageManager = getPackageManager();
+                List activities = packageManager.queryIntentActivities(webIntent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+                if (activities.size() > 0) { // found at least on app to view web page
+                    startActivity(webIntent); // start that app
+                }
             }
         });
 
@@ -59,7 +71,14 @@ public class MainActivity extends AppCompatActivity {
                         begDate.getTimeInMillis());
                 calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
                         endDate.getTimeInMillis());
-                startActivity(calIntent);
+
+                PackageManager packageManager = getPackageManager();
+                List activities = packageManager.queryIntentActivities(calIntent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+                if (activities.size() > 0) { // found at least on app to view web page
+                    startActivity(calIntent); // start that app
+                }
+
 
             }
         });
@@ -72,6 +91,166 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Do something in response to button click
                 textViewStatus.setText("Code should display map for performance 1");
+                Uri location = Uri.parse("geo:0,0?q=CSS campus, 1200 Kenwood Ave, Duluth, MN");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+
+                PackageManager packageManager = getPackageManager();
+                List activities = packageManager.queryIntentActivities(mapIntent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+                if (activities.size() > 0) { // found at least on app to view web page
+                    startActivity(mapIntent); // start that app
+                }
+
+            }
+        });
+
+
+    }
+    private void setupButtonClickEvents2() {
+        /**
+         *   Set up button click event listener for the web info button for the first performance
+         */
+        btn1Web = (Button) findViewById(R.id.button2WebInfo);
+        btn1Web.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should display website for performance 1");
+                Uri webpage = Uri.parse("http://www.css.edu/about/spotlight-arts-and-lectures/calendar.html?trumbaEmbed=eventid%3d141706941%26view%3devent%26-childview%3d");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+
+                PackageManager packageManager = getPackageManager();
+                List activities = packageManager.queryIntentActivities(webIntent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+                if (activities.size() > 0) { // found at least on app to view web page
+                    startActivity(webIntent); // start that app
+                }
+            }
+        });
+
+        /**
+         *   Set up button click event listener for the celendar button for the first performance
+         *       Use the calendar intent to set an event reminder on the calendar
+         */
+        but1Calendar = (Button) findViewById(R.id.button2Calendar);
+        but1Calendar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should set a calendar event for performance 1");
+                Intent calIntent = new Intent(Intent.ACTION_INSERT);
+                calIntent.setType("vnd.android.cursor.item/event");
+                calIntent.putExtra(CalendarContract.Events.TITLE, "Death and Desire in Dickinson and Rossetti");
+                calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Tower Hall 4119");
+                GregorianCalendar begDate = new GregorianCalendar(2017, 2, 24, 3, 40);
+                GregorianCalendar endDate = new GregorianCalendar(2017, 2, 24, 4, 40);
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                        begDate.getTimeInMillis());
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                        endDate.getTimeInMillis());
+
+                PackageManager packageManager = getPackageManager();
+                List activities = packageManager.queryIntentActivities(calIntent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+                if (activities.size() > 0) { // found at least on app to view web page
+                    startActivity(calIntent); // start that app
+                }
+
+
+            }
+        });
+
+        /**
+         *   Set up button click event listener for the web info button for the first performance
+         */
+        btn1Map = (Button) findViewById(R.id.button2map);
+        btn1Map.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should display map for performance 1");
+                Uri location = Uri.parse("geo:0,0?q=CSS campus, 1200 Kenwood Ave, Duluth, MN");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+
+                PackageManager packageManager = getPackageManager();
+                List activities = packageManager.queryIntentActivities(mapIntent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+                if (activities.size() > 0) { // found at least on app to view web page
+                    startActivity(mapIntent); // start that app
+                }
+
+            }
+        });
+
+
+    }
+    private void setupButtonClickEvents3() {
+        /**
+         *   Set up button click event listener for the web info button for the first performance
+         */
+        btn1Web = (Button) findViewById(R.id.button3WebInfo);
+        btn1Web.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should display website for performance 1");
+                Uri webpage = Uri.parse("http://www.css.edu/about/spotlight-arts-and-lectures/calendar.html?trumbaEmbed=eventid%3d141080384%26view%3devent%26-childview%3d");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+
+                PackageManager packageManager = getPackageManager();
+                List activities = packageManager.queryIntentActivities(webIntent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+                if (activities.size() > 0) { // found at least on app to view web page
+                    startActivity(webIntent); // start that app
+                }
+            }
+        });
+
+        /**
+         *   Set up button click event listener for the celendar button for the first performance
+         *       Use the calendar intent to set an event reminder on the calendar
+         */
+        but1Calendar = (Button) findViewById(R.id.button3Calendar);
+        but1Calendar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should set a calendar event for performance 2");
+                Intent calIntent = new Intent(Intent.ACTION_INSERT);
+                calIntent.setType("vnd.android.cursor.item/event");
+                calIntent.putExtra(CalendarContract.Events.TITLE, "CANCELED - High School Band Exchange Concert");
+                calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Tower Hall 4119");
+                GregorianCalendar begDate = new GregorianCalendar(2017, 2, 24, 3, 40);
+                GregorianCalendar endDate = new GregorianCalendar(2017, 2, 24, 4, 40);
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                        begDate.getTimeInMillis());
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                        endDate.getTimeInMillis());
+
+                PackageManager packageManager = getPackageManager();
+                List activities = packageManager.queryIntentActivities(calIntent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+                if (activities.size() > 0) { // found at least on app to view web page
+                    startActivity(calIntent); // start that app
+                }
+
+
+            }
+        });
+
+        /**
+         *   Set up button click event listener for the web info button for the first performance
+         */
+        btn1Map = (Button) findViewById(R.id.button1map);
+        btn1Map.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should display map for performance 3");
+                Uri location = Uri.parse("geo:0,0?q=CSS campus, 1200 Kenwood Ave, Duluth, MN");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+
+                PackageManager packageManager = getPackageManager();
+                List activities = packageManager.queryIntentActivities(mapIntent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+                if (activities.size() > 0) { // found at least on app to view web page
+                    startActivity(mapIntent); // start that app
+                }
+
             }
         });
 
